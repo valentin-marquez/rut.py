ada74a736987c9a925ca790ebeb97a858e2314e6
Valentin Marquez <valentin13.mail@gmail.com>
2023-06-04 22:54:05 -0400

    fix issues

--- rutpy/setup.py
+++ rutpy/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rutpy',
-    version='1.0.0',
+    version='1.0.1',
     description='Librería para el manejo de RUTs chilenos',
     author='Valentin Marquez',
     author_email='Valentin13.mail@gmail.com',
